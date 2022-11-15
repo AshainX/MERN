01b4fb8c133e01642bc60ca8d7bfab164c3ff198
Ashutosh Swain <ashutosh.ashain@gmail.com>
2022-11-15 10:01:19 -0800

    explicit infos

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3500
 
-app.use('/', express.static(path.join(__dirname,'/public')))
+app.use(express.json())
+
+
+//app.use('/', express.static(path.join(__dirname,'public'))) //
+app.use(express.static('public'))
 //app.listen(PORT, () => console.log(`server running on port ${PORT}`))
 
 app.use('/', require('./routes/root'))
