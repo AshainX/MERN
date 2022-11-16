b9e48b2d6031a298d1dc7897d580cff8afe26884
Ashutosh Swain <ashutosh.ashain@gmail.com>
2022-11-15 23:39:35 -0800

    added cors for api req and postmanApi success

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ const app = express()
 const path = require('path')
 const { logger } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const PORT = process.env.PORT || 3500
 
 app.use(logger)
 
+app.use(cors())
+
 app.use(express.json())
 
-//app.use('/', express.static(path.join(__dirname,'public'))) //
-app.use(express.static('public'))
-//app.listen(PORT, () => console.log(`server running on port ${PORT}`))
+app.use(cookieParser())
 
+app.use('/', express.static(path.join(__dirname,'public'))) //
+//app.use(express.static('public'))
+
+//app.listen(PORT, () => console.log(`server running on port ${PORT}`))
 app.use('/', require('./routes/root'))
 
 app.all('*', (req, res) => {
